f20f17600d78c5561197d3e934629201ac65c092
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2023-01-06 10:16:47 -0500

    api 4

--- API-Project/js/main.js
+++ API-Project/js/main.js
@@ -1,3 +1,10 @@
+const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+}
+getData(URL);
 const DOMSelector = {
   search: document.getElementById("search-buttons"),
 };
