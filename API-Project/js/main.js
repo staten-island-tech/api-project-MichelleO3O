c374a2fb97632cd0076c2912912d0d985d9928fc
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2023-01-12 10:18:44 -0500

    api 5

--- API-Project/js/main.js
+++ API-Project/js/main.js
@@ -1,10 +1,24 @@
+import "../styles/style.css";
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-
 async function getData(URL) {
-  const response = await fetch(URL);
-  console.log(response);
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent =
+        data.data[0].entry[0].title;
+      `<div class = 'card>
+          <h2 class = "title">${title}</h2>
+          <img src = "${Image}' alt = "" />
+          </div>`;
+    }
+    const data = await response.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
 getData(URL);
-const DOMSelector = {
-  search: document.getElementById("search-buttons"),
-};
